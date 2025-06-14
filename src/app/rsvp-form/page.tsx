46c764d135aeb4f153099ab99e60747a272325cc
Temiloluwa <temilolu74@gmail.com>
2025-06-14 03:04:02 +0200

    first draft that looks like it has sense

--- src/app/rsvp-form/page.tsx
+++ src/app/rsvp-form/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
 import RSVPForm from '../../components/RSVPForm';
 
-export default RSVPForm;
+const RSVPFormPage = () => {
+  const handleRSVPSubmit = (name: string, attending: boolean, message: string) => {
+    // TODO: Implement submission logic (e.g., call an API route)
+    console.log('RSVP submitted:', { name, attending, message });
+  };
+
+  return <RSVPForm onSubmit={handleRSVPSubmit} />;
+};
+
+export default RSVPFormPage;
